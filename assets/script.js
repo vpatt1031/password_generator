75681d39c44a0b3f74f3acf5f31331e94951d8e3
Vanessa Patterson <vpatt1031@gmail.com>
2020-12-05 17:21:31 -0500

    added arrays to script.js

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
-//properties for passwordText variable
-var passwordText = {
-    lowercase: "abcdefghijklmnoqrstuvwyz",
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUV",
-    numbers: 0123456789,
-    specials: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-}
+//Assigned variables for password characters in arrays as strings.
+
+    var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "q", "r", "s", "t", "u", "v", "w", "y", "z"];
+    var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"];
+    var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var specialChar = !#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+    
 
 
 
@@ -19,8 +19,13 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  //call the function
-  passwordText.writePassword();
+
+
+
+
+
+  
+  
 }
 
 // Add event listener to generate button
@@ -33,27 +38,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-function password_generator( len ) {
-    var length = (len)?(len):(10);
-    var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-    var numeric = '0123456789';
-    var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-    var password = "";
-    var character = "";
-    var crunch = true;
-    while( password.length<length ) {
-        entity1 = Math.ceil(string.length * Math.random()*Math.random());
-        entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
-        entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
-        hold = string.charAt( entity1 );
-        hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
-        character += hold;
-        character += numeric.charAt( entity2 );
-        character += punctuation.charAt( entity3 );
-        password = character;
-    }
-    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-    return password.substr(0,len);
-}
-
-console.log( password_generator() );
